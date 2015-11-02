82b3674aeeedab7b952561d356150b0b77f61385
Gustav Davidsson <g.davidsson@hotmail.com>
2015-11-02 00:43:08 -0500

    Covered new cases of energy averaging script

--- one_1.5.1-RC2/process_energy_report.py
+++ one_1.5.1-RC2/process_energy_report.py
@@ -2,14 +2,16 @@ import sys
 import os
 
 errtemplate = "Failed to create average energy report as "
+ENERGY_LEVEL_REPORT = "EnergyLevelReport"
+DEFAULT_SETTINGS_FILE = "default_settings.txt"
 
 def process_energy_report(test_name):
-    INPUT_PATH = "reports/%s_EnergyLevelReport.txt" % test_name
+    INPUT_PATH = "reports/%s_%s.txt" % (test_name, ENERGY_LEVEL_REPORT)
     if not os.path.exists(INPUT_PATH):
         print "Failed to create average energy report as no energy report was found for scenario \"%s\"." % test_name
         return
 
-    OUTPUT_PATH = "reports/%s_AverageEnergyLevelReport.txt" % test_name
+    OUTPUT_PATH = "reports/%s_Average%s.txt" % (test_name, ENERGY_LEVEL_REPORT)
 
     f_in = open(INPUT_PATH, 'r')
     avg_level_by_type_time = {}
@@ -57,11 +59,11 @@ def process_energy_report(test_name):
 
     f_out.close()
 
-def process_file(filename):
-    if not os.path.exists(filename):
-        print "%sthe given settings file does not exist." % errtemplate
-        return False, None
+def parse_settings(filename):
     file_content = os.popen('cat %s' % filename).read()
+    scenario_name = None
+    num_reports = -1
+    reports = {}
     for line in file_content.split('\n'):
         if line.startswith("Scenario.name"):
             eq_sign_index = line.index('=')
@@ -69,14 +71,51 @@ def process_file(filename):
                 print "%sthe settings file's field with Scenario.name has no equal sign..." % errtemplate
                 return False, None
             scenario_name = line[eq_sign_index+1:].strip()
-            return True, scenario_name
-    print "%sScenario.name was not found in %s" % (errtemplate, filename)
-    return False, None
+
+        if line.startswith("Report.report"):
+            eq_sign_index = line.index('=')
+            if eq_sign_index == -1:
+                print "%sone of the settings file's fields with Report.report* has no equal sign..." % errtemplate
+                return False, None
+            if not line[13:eq_sign_index].strip().isdigit():
+                # Not a Report.reportX entry
+                continue
+            report_number = int(line[13:eq_sign_index].strip())
+            report_name = line[eq_sign_index+1:].strip()
+            if report_name not in reports:
+                reports[report_name] = []
+            reports[report_name].append(report_number)
+    return scenario_name, num_reports, reports
+
+def process_file(filename):
+    if not os.path.exists(filename):
+        print "%sthe given settings file does not exist." % errtemplate
+        return False, None
+
+    scenario_name, num_reports, reports = parse_settings(filename)
+
+    if scenario_name is None:
+        print "%sScenario.name was not found in %s" % (errtemplate, filename)
+        return False, None
+
+    if ENERGY_LEVEL_REPORT in reports:
+        return True, scenario_name
+
+    _, def_num_reports, def_reports = parse_settings(DEFAULT_SETTINGS_FILE)
+
+    if ENERGY_LEVEL_REPORT not in def_reports:
+        return False, scenario_name
+
+    energy_level_report_number = def_reports[ENERGY_LEVEL_REPORT]
+    if (energy_level_report_number not in reports.values() and
+        num_reports == -1 or energy_level_report_number <= num_reports):
+        return True, scenario_name
+    return False, scenario_name
 
 def check_generate_energy_report(sysargs):
     # Find what scenario is used
     args = [x for x in sysargs if x[-4:] == ".txt"]
-    filename = "default_settings.txt"
+    filename = DEFAULT_SETTINGS_FILE
     if len(args) == 1:
         filename = args[0]
     elif len(args) != 0:
